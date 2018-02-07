d7db733fe0bbe66c63ab58d881ddcbff165c9d27
Ismael Barajas <inxxanee@gmail.com>
2018-02-07 11:59:05 -0800

    lab2.c
    complete

--- lab2.c
+++ lab2.c
@@ -2,6 +2,43 @@
 #include <string.h>
 #include <assert.h>
 
+<<<<<<< HEAD
+int main(){
+
+	unsigned int inputIntVal = 0;
+	unsigned int pastIntVal = 0;
+
+	char strin[10];
+
+	while(1){
+	if (scanf("%s", strin) <= 0) break;
+	if (scanf(" %x", &inputIntVal) <= 0) break;
+	if (strcmp(strin, "q") == 0) break;
+	if (strcmp(strin, "set") == 0){
+		pastIntVal = inputIntVal;
+	}
+	else if (strcmp(strin, "shl") == 0) {
+		pastIntVal = pastIntVal << inputIntVal;
+	}
+	else if (strcmp(strin, "or") == 0) {
+		pastIntVal = pastIntVal | inputIntVal;
+	}
+	else if (strcmp(strin, "shr") == 0) {
+		pastIntVal = pastIntVal >> inputIntVal;
+	}
+	else if (strcmp(strin, "and") == 0) {
+		pastIntVal = pastIntVal & inputIntVal;
+	}
+	else if (strcmp(strin, "xor") == 0) {
+		pastIntVal = pastIntVal ^ inputIntVal;
+	}
+	else{
+	printf("Invalid input\n");
+	return 0;
+	}
+
+	printf("%s => 0x%x \n", strin, pastIntVal);
+=======
 void setFunc(unsigned int *z, unsigned int *y) {
 	*y = *z;
 }
@@ -62,6 +99,7 @@ int main(){
 
 	printf("\n%s => %x \n", strin, inputIntVal);
 	loopVal++;
+>>>>>>> origin/master
 	}
 	return 0;
 }
