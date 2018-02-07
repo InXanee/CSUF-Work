3c2b7e9c0b035a15b986c38cd3efcf2db0bfa12f
Ismael Barajas <inxxanee@gmail.com>
2018-02-06 22:49:18 -0500

    lab2.c
    Lab2 CPCS 223c

---
+++ lab2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <assert.h>
+
+
+
+
+int main(){
+
+	int i;
+	char strin[10];
+
+	//printf("Please enter operation and number: ");
+	while(){
+	if(scanf("%s", strin)==1){
+	}
+	if(scanf(" %d", &i)==1){
+	}
+	printf("\n%s => %d \n", strin, i);
+
+	switch(strin){
+	
+	}
+	}
+	return 0;
+}
