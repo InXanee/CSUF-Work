c89d9c0a1cd8c318700b6d08e22294f7d76db011
Ismael Barajas <inxxanee@gmail.com>
2018-02-07 01:07:41 -0500

    lab2.c

--- lab2.c
+++ lab2.c
@@ -1,25 +1,67 @@
 #include <stdio.h>
+#include <string.h>
 #include <assert.h>
 
+void setFunc(unsigned int *z, unsigned int *y) {
+	*y = *z;
+}
 
+void shlFunc(unsigned int *z, unsigned int *y) {
+	*y << *z;
+}
 
+void orFunc(unsigned int *z, unsigned int *y) {
+	*y | *z;
+}
+
+void shrFunc(unsigned int *z, unsigned int *y) {
+	*y >> *z;
+}
+
+void andFunc(unsigned int *z, unsigned int *y) {
+	*y & *z;
+}
+
+void xorFunc(unsigned int *z, unsigned int *y) {
+	*y ^ *z;
+}
 
 int main(){
 
-	int i;
+	unsigned int inputIntVal;
+	unsigned int pastIntVal;
+	int loopVal = 0;
 	char strin[10];
-
 	//printf("Please enter operation and number: ");
-	while(){
-	if(scanf("%s", strin)==1){
-	}
-	if(scanf(" %d", &i)==1){
-	}
-	printf("\n%s => %d \n", strin, i);
+	while(loopVal != 3){
 
-	switch(strin){
-	
+	if (scanf("%s", strin)==1){}
+	if (scanf(" %x", &inputIntVal)==1){}
+
+	if (strcmp(strin, "set") == 0){
+			setFunc(inputIntVal, pastIntVal);
 	}
+	else if (strcmp(strin, "shl") == 0) {
+		shlFunc(inputIntVal, pastIntVal);
+	}
+	else if (strcmp(strin, "or") == 0) {
+		orFunc(inputIntVal, pastIntVal);
+	}
+	else if (strcmp(strin, "shr") == 0) {
+		shrFunc(inputIntVal, pastIntVal);
+	}
+	else if (strcmp(strin, "and") == 0) {
+		andFunc(inputIntVal, pastIntVal);
+	}
+	else if (strcmp(strin, "xor") == 0) {
+		xorFunc(inputIntVal, pastIntVal);
+	}
+	else{
+
+	}
+
+	printf("\n%s => %x \n", strin, inputIntVal);
+	loopVal++;
 	}
 	return 0;
 }
